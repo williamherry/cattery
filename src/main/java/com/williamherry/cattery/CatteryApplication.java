badc85c6f175a4657f3baf97b28a0436a75869f6
William Herry <WilliamHerryChina@Gmail.com>
2020-11-29 14:10:20 +0800

    first commit

---
+++ src/main/java/com/williamherry/cattery/CatteryApplication.java
@@ -0,0 +1,13 @@
+package com.williamherry.cattery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatteryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CatteryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/williamherry/cattery/CatteryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.williamherry.cattery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatteryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
